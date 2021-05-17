5c0374efb60b189bfca4881d0ab13fd6496e2320
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2021-05-17 22:55:07 +0700

    Initiate omdb projects

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <b-container>
+    <h1>This is home page ...</h1>
+  </b-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
